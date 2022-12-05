90c87d981ec920adbf3f8d139aaa66897db20493
The0x539 <the0x539@gmail.com>
2022-12-05 00:45:52 -0600

    Add fist and gun icons

--- Plugin.cs
+++ Plugin.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.IO;
+﻿using System;
 
 using BepInEx;
 
@@ -14,12 +13,19 @@ namespace LeatHud;
 [BepInDependency("zed.uk.uihelper")]
 public sealed class Plugin : BaseUnityPlugin {
     private GameObject? wheel;
+    private GameObject? gun;
+    private GameObject? fist;
+
+    private readonly float gunScale = 0.1f;
+    private readonly float fistScale = 1.25f;
+
+    private float iconTimer = 0f;
 
     public void Awake() {
         this.Logger.LogInfo("Plugin loaded!");
     }
 
-    private GameObject MakeWheel(Crosshair crosshair) {
+    private static GameObject MakeWheel(Crosshair crosshair) {
         var wheel = UIHelper.CreateImage();
 
         wheel.AddComponent<Slider>();
@@ -48,7 +54,6 @@ public sealed class Plugin : BaseUnityPlugin {
         image.fillMethod = Image.FillMethod.Radial360;
         image.type = Image.Type.Filled;
         image.fillAmount = 0.0f;
-        //image.sprite = MonoSingleton<PowerUpMeter>.Instance.GetComponent<Image>().sprite;
 
         wheel.AddComponent<SliderToFillAmount>();
         var stfa = wheel.GetComponent<SliderToFillAmount>();
@@ -63,23 +68,118 @@ public sealed class Plugin : BaseUnityPlugin {
         return wheel;
     }
 
+    private static GameObject MakeGun() {
+        var gun = UIHelper.CreateImage();
+
+        var canvas = UIHelper.CreateOverlay(persistent: true);
+        canvas.GetComponent<Canvas>().sortingOrder = 10;
+
+        var rt = gun.GetComponent<RectTransform>();
+        rt.SetParent(canvas.GetComponent<RectTransform>());
+        rt.SetPivot(PivotPresets.MiddleLeft);
+        rt.anchoredPosition = new Vector2(45, 0);
+
+        gun.SetActive(true);
+
+        return gun;
+    }
+
+    private static float BattleVolume() {
+        var musicManager = MonoSingleton<MusicManager>.Instance;
+        return Math.Max(musicManager.battleTheme.volume, musicManager.bossTheme.volume);
+    }
+
+    private static GameObject MakeFist() {
+        var fist = UIHelper.CreateImage();
+
+        var canvas = UIHelper.CreateOverlay(persistent: true);
+        canvas.GetComponent<Canvas>().sortingOrder = 10;
+
+        var rt = fist.GetComponent<RectTransform>();
+        rt.SetParent(canvas.GetComponent<RectTransform>());
+        rt.SetPivot(PivotPresets.MiddleRight);
+        rt.anchoredPosition = new Vector2(-50, 0);
+
+        fist.SetActive(true);
+
+        return fist;
+    }
+
     public void Update() {
+        this.UpdateWheel();
+        this.UpdateIconTimer();
+        this.UpdateGun();
+        this.UpdateFist();
+    }
+
+    private void UpdateIconTimer() {
+        var musicManager = MonoSingleton<MusicManager>.Instance;
+        var battleVolume = Math.Max(musicManager.battleTheme.volume, musicManager.bossTheme.volume);
+
+        if (battleVolume > 0) {
+            this.iconTimer = 5;
+        } else {
+            this.iconTimer = Mathf.MoveTowards(this.iconTimer, 0, Time.deltaTime);
+        }
+    }
+
+    private void UpdateWheel() {
+        var crosshair = MonoSingleton<HUDOptions>.Instance?.GetComponentInChildren<Crosshair>();
         if (this.wheel == null) {
-            var crosshair = FindObjectOfType<Crosshair>();
             if (crosshair == null) {
                 return;
             }
 
-            this.wheel = this.MakeWheel(crosshair);
+            this.wheel = MakeWheel(crosshair);
         }
 
-        this.wheel.SetActive(FindObjectOfType<Crosshair>() != null);
+        this.wheel.SetActive(crosshair != null);
 
         var wc = MonoSingleton<WeaponCharges>.Instance;
-        var cbs = MonoSingleton<ColorBlindSettings>.Instance;
         if (wc != null) {
+            var cbs = MonoSingleton<ColorBlindSettings>.Instance;
             this.wheel.GetComponent<Slider>().value = wc.raicharge;
             this.wheel.GetComponent<Image>().color = wc.raicharge < 4 ? cbs.railcannonChargingColor : cbs.railcannonFullColor;
+        }
+    }
+
+    private void UpdateGun() {
+        if (this.gun == null) {
+            this.gun = MakeGun();
+        }
+
+        var weaponHud = MonoSingleton<WeaponHUD>.Instance;
+
+        this.gun.SetActive(weaponHud != null);
+
+        if (weaponHud != null) {
+            var img = this.gun.GetComponent<Image>();
+            var rt = this.gun.GetComponent<RectTransform>();
+            var icon = weaponHud.GetComponentInChildren<Image>();
+
+            img.sprite = icon.sprite;
+            img.color = icon.color with { a = Math.Min(1, this.iconTimer) };
+            rt.sizeDelta = new Vector2(img.sprite.texture.width, img.sprite.texture.height) * this.gunScale;
+        }
+    }
+
+    private void UpdateFist() {
+        if (this.fist == null) {
+            this.fist = MakeFist();
+        }
+
+        var fistControl = MonoSingleton<FistControl>.Instance;
+        var fistIcon = fistControl?.fistIcon;
+
+        this.fist.SetActive(fistIcon != null && fistControl!.activated);
+
+        if (fistIcon != null) {
+            var img = this.fist.GetComponent<Image>();
+            var rt = this.fist.GetComponent<RectTransform>();
+
+            img.sprite = fistIcon.sprite;
+            img.color = fistIcon.color with { a = Math.Min(1, this.iconTimer) };
+            rt.sizeDelta = new Vector2(img.sprite.texture.width, img.sprite.texture.height) * this.fistScale;
         }
     }
 }
