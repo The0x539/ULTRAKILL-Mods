ea4232d611332e6256341b26a27fa03dc67d99bd
The0x539 <the0x539@gmail.com>
2023-04-28 15:31:09 -0500

    BetterWeaponWheel: Remake for Patch 13

--- BetterWeaponWheel/Plugin.cs
+++ BetterWeaponWheel/Plugin.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Reflection;
 using System.Collections.Generic;
 
@@ -17,64 +16,62 @@ public sealed class Plugin : BaseUnityPlugin {
         Harmony.CreateAndPatchAll(this.GetType());
     }
 
-    public void Start() {
-        GunWheelOverlay.SetLogger(this.Logger);
-    }
-
-    public void Update() {
-        _ = GunWheelOverlay.Instance;
-    }
-
-    [HarmonyPrefix]
-    [HarmonyPatch(typeof(WeaponWheel), "SetSegmentCount")]
-    static void OverrideWheelSegmentCount(ref int count) {
-        count = GunControl.Instance.slots.Count(slot => slot.Any());
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(WeaponWheel), "Update")]
+    static void UpdateWheelCursor(ref Vector2 ___direction) {
+        WheelDot.Instance.UpdateCursor(___direction);
     }
 
     [HarmonyPostfix]
-    [HarmonyPatch(typeof(WeaponWheel), "SetSegmentCount")]
-    static void ReinitializeGunWheelWheelIcons(int count) {
-        GunWheelOverlay.Instance.InitWheelIcons(count);
-    }
-
-    private static bool weaponWheelIsUpdating = false;
-
-    [HarmonyPrefix]
-    [HarmonyPatch(typeof(WeaponWheel), "Update")]
-    static void WeaponWheelUpdatePrefix() {
-        weaponWheelIsUpdating = true;
-    }
-
-    [HarmonyPostfix]
-    [HarmonyPatch(typeof(WeaponWheel), "Update")]
-    static void WeaponWheelUpdatePostfix(ref int ___selectedSegment, ref Vector2 ___direction) {
-        GunWheelOverlay.Instance.UpdateWheelInfo(___selectedSegment, ___direction);
-        weaponWheelIsUpdating = false;
-    }
-
-    [HarmonyPrefix]
-    [HarmonyPatch(typeof(GunControl), "SwitchWeapon", typeof(int))]
-    static void AdjustSelectedWeapon(GunControl __instance, ref int target) {
-        if (!weaponWheelIsUpdating) {
+    [HarmonyPatch(typeof(WheelSegment), nameof(WheelSegment.SetActive))]
+    static void OverrideSegmentColors(WheelSegment __instance, ref bool active) {
+        if (!active) {
             return;
         }
 
-        var gunc = __instance;
-
-        var nonEmptySlotIndices = new List<int>(8);
-        for (var i = 0; i < gunc.slots.Count; i++) {
-            if (gunc.slots[i].Any()) {
-                nonEmptySlotIndices.Add(i);
-            }
-        }
-
-        target = nonEmptySlotIndices[target - 1] + 1;
+        var variantIdx = (int)__instance.descriptor.variationColor;
+        var color = ColorBlindSettings.Instance.variationColors[variantIdx];
+        __instance.icon.color = color;
+        __instance.iconGlow.color = color;
     }
+
+    private static readonly FieldInfo segmentsField = typeof(WeaponWheel).GetField("segments", BindingFlags.Instance | BindingFlags.NonPublic);
 
     [HarmonyPostfix]
-    [HarmonyPatch(typeof(WeaponWheel), "OnEnable")]
-    [HarmonyPatch(typeof(WeaponWheel), "OnDisable")]
-    static void ReinitializeGunWheelIconScale() {
-        GunWheelOverlay.Instance.ResetIconScales();
+    [HarmonyPatch(typeof(GunControl), nameof(GunControl.SwitchWeapon), typeof(int), typeof(List<GameObject>), typeof(bool), typeof(bool), typeof(bool))]
+    [HarmonyPatch(typeof(WeaponWheel), nameof(WeaponWheel.SetSegments))]
+    private static void OverrideWheelIcons() {
+        var gunc = GunControl.Instance;
+        var segments = (List<WheelSegment>)segmentsField.GetValue(WeaponWheel.Instance);
+
+        var segmentIdx = 0;
+        for (var slotIdx = 0; slotIdx < gunc.slots.Count; slotIdx++) {
+            var slot = gunc.slots[slotIdx];
+            if (!slot.Any()) {
+                continue;
+            }
+
+            int variantIdx;
+            if (slotIdx + 1 == gunc.currentSlot) {
+                variantIdx = (gunc.currentVariation + 1) % slot.Count;
+            } else if (gunc.variationMemory) {
+                variantIdx = PlayerPrefs.GetInt($"Slot{slotIdx + 1}Var", 0);
+            } else {
+                variantIdx = 0;
+            }
+
+            var segment = segments[segmentIdx++];
+            var descriptor = slot[variantIdx].GetComponent<WeaponIcon>().weaponDescriptor;
+            if (segment.descriptor == descriptor) {
+                // this segment is already up to date
+                continue;
+            }
+
+            segment.descriptor = descriptor;
+            segment.icon.sprite = descriptor.icon;
+            segment.iconGlow.sprite = descriptor.glowIcon;
+        }
     }
+
+    // TODO: allow switching other slots' weapons while the wheel is up?
 }
---
+++ BetterWeaponWheel/WheelDot.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace BetterWeaponWheel;
+
+public sealed class WheelDot : MonoSingleton<WheelDot> {
+    private bool initialized = false;
+    private Vector2 position;
+
+    private GameObject overlay = new();
+    private GameObject wheelDot = new();
+
+    public void Update() {
+        if (!this.TryInit()) {
+            return;
+        }
+
+        var active = WeaponWheel.Instance.isActiveAndEnabled;
+        this.overlay.SetActive(active);
+        this.wheelDot.SetActive(active);
+        if (!active) {
+            return;
+        }
+
+        this.wheelDot.GetComponent<RectTransform>().anchoredPosition = this.position * 71;
+    }
+
+    private bool TryInit() {
+        if (this.initialized) {
+            return true;
+        }
+
+        this.InitOverlay();
+        this.InitWheelDot();
+        this.initialized = true;
+        return true;
+    }
+
+    private void InitOverlay() {
+        var overlay = new GameObject("Overlay");
+
+        var canvas = overlay.AddComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        canvas.sortingOrder = 50;
+
+        var scaler = overlay.AddComponent<CanvasScaler>();
+        scaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+        scaler.screenMatchMode = CanvasScaler.ScreenMatchMode.MatchWidthOrHeight;
+        scaler.matchWidthOrHeight = 0;
+        scaler.referenceResolution = new(1920, 1080);
+
+        var group = overlay.AddComponent<CanvasGroup>();
+        group.blocksRaycasts = false;
+
+        this.overlay = overlay;
+    }
+
+    private void InitWheelDot() {
+        this.wheelDot = new GameObject();
+
+        this.wheelDot.SetActive(false);
+
+        var img = this.wheelDot.AddComponent<Image>();
+        img.sprite = null;
+
+        var crosshairColorPref = PrefsManager.Instance.GetInt("crossHairColor");
+
+        img.color = crosshairColorPref switch {
+            0 or 1 => Color.white,
+            2 => Color.gray,
+            3 => Color.black,
+            4 => Color.red,
+            5 => Color.green,
+            6 => Color.blue,
+            7 => Color.cyan,
+            8 => Color.yellow,
+            9 => Color.magenta,
+            _ => Color.white,
+        };
+
+        if (crosshairColorPref == 0) {
+            var crosshair = StatsManager.Instance.crosshair.GetComponent<Crosshair>();
+            img.material = crosshair.invertMaterial;
+        }
+
+        var rt = this.wheelDot.GetComponent<RectTransform>();
+        rt.SetParent(this.overlay.GetComponent<RectTransform>());
+        rt.pivot = rt.anchorMin = rt.anchorMax = new(0.5f, 0.5f);
+        rt.sizeDelta = new(3, 3);
+        rt.Rotate(0, 0, 45);
+    }
+
+    public void UpdateCursor(Vector2 direction) {
+        this.position = direction;
+    }
+}
