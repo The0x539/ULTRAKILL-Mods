4097c6b3cd33137f24113a3767e4df2b6c29d0c4
The0x539 <the0x539@gmail.com>
2023-04-28 13:53:48 -0500

    IntroSkip: Fix for Patch 13

--- IntroSkip/Plugin.cs
+++ IntroSkip/Plugin.cs
@@ -8,20 +8,24 @@ namespace IntroSkip;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public sealed class Plugin : BaseUnityPlugin {
+    private static string targetScene = "Main Menu";
+
     public Plugin() {
         if (SceneManager.GetActiveScene().name != "Bootstrap") {
             return;
         }
 
-        var targetScene = this.Config.Bind("IntroSkip", "TargetScene", "Main Menu");
-        SceneHelper.LoadScene(targetScene.Value);
+        var configEntry = this.Config.Bind("IntroSkip", "TargetScene", "Main Menu");
+        targetScene = configEntry.Value;
         Harmony.CreateAndPatchAll(this.GetType());
     }
 
     [HarmonyPatch(typeof(IntroViolenceScreen), "Start")]
     [HarmonyPrefix]
-    public static bool KillIntro(IntroViolenceScreen __instance) {
+    public static bool KillIntro(IntroViolenceScreen __instance, ref GameObject ___loadingScreen) {
+        SceneHelper.LoadScene(targetScene);
         __instance.gameObject.SetActive(false);
+        ___loadingScreen.SetActive(true);
         Cursor.visible = false;
         return false;
     }
