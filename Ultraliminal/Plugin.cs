abcc516f803ba0878021cfe9d3869117fa96c46c
The0x539 <the0x539@gmail.com>
2024-01-16 18:03:22 -0600

    ULTRALIMINAL: initial commit

---
+++ Ultraliminal/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Ultraliminal;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public sealed class Plugin : BaseUnityPlugin {
+    public void Awake() {
+        Harmony.CreateAndPatchAll(this.GetType());
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(EnemyIdentifier), "Awake")]
+    static void AddForcedPerspective(EnemyIdentifier __instance) {
+        __instance.gameObject.AddComponent<ForcedPerspective>();
+    }
+}
+
+sealed class ForcedPerspective : MonoBehaviour {
+    private Vector3 baseScale;
+    private float baseDistance;
+
+    public void Awake() {
+        this.baseScale = this.gameObject.transform.localScale;
+        this.baseDistance = this.ComputeDistance();
+    }
+
+    public void Update() {
+        var scaleFactor = this.ComputeDistance() / this.baseDistance;
+        this.transform.localScale = scaleFactor * this.baseScale;
+    }
+
+    private float ComputeDistance() {
+        var camPos = Camera.main.transform.position;
+        var ownPos = this.transform.position;
+        return (ownPos - camPos).magnitude;
+    }
+}
---
+++ Ultraliminal/README.md
@@ -0,0 +1,2 @@
+# ULTRALIMINAL
+Don't you hate how things get smaller when you move away from them?
