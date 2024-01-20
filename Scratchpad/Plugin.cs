00b13c8075eee6f5aa9b0c487d4c113942abefa4
The0x539 <the0x539@gmail.com>
2024-01-20 12:03:24 -0600

    Add a scratchpad mod for general experimentation

---
+++ Scratchpad/Plugin.cs
@@ -0,0 +1,12 @@
+﻿using BepInEx;
+
+using HarmonyLib;
+
+namespace Scratchpad;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public sealed class Plugin : BaseUnityPlugin {
+    public void Awake() {
+        Harmony.CreateAndPatchAll(this.GetType());
+    }
+}
