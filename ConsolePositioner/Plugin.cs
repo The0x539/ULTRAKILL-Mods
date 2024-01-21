b38263798ec05c7c1e6c98aece3464ded3d0ae6c
The0x539 <the0x539@gmail.com>
2024-01-21 13:08:26 -0600

    ConsolePositioner: initial commit

---
+++ ConsolePositioner/Plugin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+using BepInEx;
+
+namespace ConsolePositioner;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public sealed class Plugin : BaseUnityPlugin {
+    public void Awake() {
+        SetWindowPos(GetConsoleWindow(), 0, -760, 10, 750, 1000, 0x0014);
+    }
+
+    [DllImport("kernel32.dll")]
+    static extern IntPtr GetConsoleWindow();
+
+    [DllImport("user32.dll")]
+    static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, uint uflags);
+}
